e6b88731a51b2c8ce6bd549dde6a7e6a2c5ac979
Mark Prucnal <markprucnal18@gmail.com>
2023-03-11 22:09:55 -0500

    Path update

--- src/train_bennu.py
+++ src/train_bennu.py
@@ -19,7 +19,7 @@ physical_devices = tf.config.list_physical_devices('GPU')
 print(physical_devices)
 
 #CHANGE USERPATH
-USERPATH = 'C:\\Users\\Mark Prucnal\\Desktop\\bennu'
+USERPATH = '/Users/Mark Prucnal/Desktop/bennu'
 
 def main():
     model_name = 'YOCOv0.1-ailBennu'
